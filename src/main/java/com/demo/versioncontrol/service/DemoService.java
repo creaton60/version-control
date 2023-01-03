088dcdff28fa337af2879e0af287097f3277151f
creaton60 <lovmistake@gmail.com>
2023-01-03 10:20:07 +0900

    [create] demo service

---
+++ src/main/java/com/demo/versioncontrol/service/DemoService.java
@@ -0,0 +1,9 @@
+package com.demo.versioncontrol.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class DemoService implements DemoOperationUseCase{
+}
