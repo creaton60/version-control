44915c40919cf5687621ad63845a150753787019
creaton60 <lovmistake@gmail.com>
2023-01-03 09:42:40 +0900

    [create] Demo Controller Class for live demo

---
+++ src/main/java/com/demo/versioncontrol/ui/DemoController.java
@@ -0,0 +1,11 @@
+package com.demo.versioncontrol.ui;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class DemoController {
+
+
+}
